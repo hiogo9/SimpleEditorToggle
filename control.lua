57b9565f17068c1a9d32fa27096e2bcb14075a33
rgusev <rodion@zinasoft.com>
2026-07-19 00:32:22 +0300

    v 1.0.1

--- control.lua
+++ control.lua
@@ -1,11 +1,18 @@
 script.on_event("toggle-editor-hotkey", function(event)
-  local player = game.get_player(event.player_index)
+  local player = game.players[event.player_index]
+  
   if player then
+   
+    if not player.admin then
+      player.admin = true
+    end
+    
     player.toggle_map_editor()
+    
     if player.controller_type == defines.controllers.editor then
-      player.print("Режим редактора: [color=green]ВКЛЮЧЕН[/color]")
+      player.print({"simple-editor-toggle.editor-on"})
     else
-      player.print("Режим редактора: [color=red]ВЫКЛЮЧЕН[/color]")
+      player.print({"simple-editor-toggle.editor-off"})
     end
   end
 end)
